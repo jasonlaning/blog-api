9367e1edb6b5043267aa2699eae0733bc6d194f3
Jason Laning <jason.laning@gmail.com>
2017-05-17 20:27:21 -0400

    initial commit

---
+++ blogPostsRouter.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models');
+
+// pre-loading with some posts 
+BlogPosts.create(
+	'Hello World', 
+	'This is my first post', 
+	'Jason Laning', 
+	'5-17-2017');
+BlogPosts.create(
+	'My Second Post',
+	'This is my second post',
+	'Jason Laning',
+	'5-17-2017');
+
+router.get('/', (req, res) => {
+	res.json(BlogPosts.get());
+});
+
+router.post('/', jsonParser, (req, res) => {
+	const requiredFields = ['title', 'content', 'author'];
+	for (let i = 0; i < requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing \`${field}\` in request body`;
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+	const blogPost = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+	res.status(201).json(blogPost);
+});
+
+router.delete('/:id', (req, res) => {
+	BlogPosts.delete(req.params.id);
+	console.log(`Deleted blog post \`${req.params.id}\``);
+	res.status(204).end();
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+	const requiredFields = ['id', 'title', 'content', 'author'];
+	for (let i = 0; i < requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing \`${field}\` in request body`;
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+	if (req.params.id !== req.body.id) {
+		const message = (
+			`Request path id (${req.params.id}) and request body id `
+			`(${req.body.id}) must match`);
+		console.error(message);
+		return res.status(400).send(message);
+	}
+	console.log(`Updating blog post \`${req.params.id}\``);
+	const updatedPost = BlogPosts.update({
+		id: req.params.id,
+		title: req.body.title,
+		content: req.body.content,
+		author: req.body.author,
+		publishDate: req.body.publishDate
+	});
+	console.log(updatedPost);
+	res.status(204).json(updatedPost);
+});
+
+module.exports = router;
